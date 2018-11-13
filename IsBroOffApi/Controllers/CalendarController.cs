f52789f3a3bca7f08e572eeace24a9dcc7ddfbd4
Sean Schwabenlander <sean@schwabenlander.com>
2018-11-13 15:44:32 +0300

    Initial API setup. Created DTO model. Added Calendar Controller.

---
+++ IsBroOffApi/Controllers/CalendarController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IsBroOffApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IsBroOffApi.Controllers
+{
+    [Route("api/calendar")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        // GET api/calendar/2018/11/13
+        [HttpGet("{yyyy}/{mm}/{dd}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<WorkStatusDto> GetWorkStatusForDate(int yyyy, int mm, int dd)
+        {
+            DateTime.TryParse($"{yyyy}-{mm}-{dd}", out var date);
+
+            if (date == DateTime.MinValue)
+                return BadRequest();
+
+            return new WorkStatusDto
+            {
+                Date = date,
+                DateAsString = date.ToLongDateString(),
+                IsBroOff = true,
+                StatusText = "Bro is off today"
+            };
+        }
+    }
+}
---
+++ IsBroOffApi/Models/WorkStatusDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsBroOffApi.Models
+{
+    public class WorkStatusDto
+    {
+        public DateTime Date { get; set; }
+        public string DateAsString { get; set; }
+        public bool IsBroOff { get; set; }
+        public string StatusText { get; set; }
+    }
+}
